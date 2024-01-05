c736f9677f12fc55ca39853e1859845d57b53d7a
monindew <yi.guni@gmail.com>
2024-01-05 09:16:53 +0900

    토탈매출계산기

--- chicken_price.py
+++ chicken_price.py
@@ -1,7 +1,12 @@
 with open("data/chicken.txt", "r", encoding='UTF-8') as f:
+    total_machul = 0
+    days = 0
     for line in f:
         data = line.strip().split(": ")
-        machul = data[1]
-    total_machul = 0
-    total_days = 0
+        machul = int(data[1])
+        total_machul += machul
+        days += 1
+    print(total_machul)
+    print(total_machul/days)
+    
     
