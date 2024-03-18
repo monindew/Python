662d5d82deefb1c4914c6ab22104bd2f77dfb844
monindew <yi.guni@gmail.com>
2024-03-18 20:25:25 +0900

    ,

---
+++ 연습.py
@@ -0,0 +1,10 @@
+fruits = ['apple', 'pear', 'banana']
+i = 0
+
+while i < len(fruits):
+    if fruits[i] == 'banana':
+        print("바나나의 위치:", i)
+        break
+    else:
+        i += 1 
+        
