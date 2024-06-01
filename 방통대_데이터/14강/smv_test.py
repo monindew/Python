a9305df63eaece8bc763f04cfec218b1aac6fdb7
monindew <yi.guni@gmail.com>
2024-06-01 12:27:38 +0900

    ...

---
+++ 방통대_데이터/14강/smv_test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import sklearn.svm as svm
+
+from sklearn.model_selection import cross_val_score
+
+
+df = pd.read_csv('train.csv')
+
+#데이터 형식 변경
+
+df['HighPrice'] = (df['SalePrice'] >= 100000).astype(int)
+
+X = df[['LotArea', 'OverallQual', 'OverallCond', 'YearBuilt']]
+y = df['HighPrice']
+
+svm_classifier = svm.SVC(kernel='linear')
+result = svm_classifier.fit(X,y)
+print(result.predict(X))
